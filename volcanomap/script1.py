28079281fd6d6902ce63549752cb988c7367bc7b
D.M. Cook <dmaterialized@gmail.com>
2017-02-25 00:16:05 -0500

    add stepping and averaging of viewport

--- volcanomap/script1.py
+++ volcanomap/script1.py
@@ -1,16 +1,26 @@
 #creates volcano map
 import folium
 import pandas
-
 # initialize pandas
 df=pandas.read_csv("Volcanoes-USA.txt")
-
 #initialize folium
-map=folium.Map(location=[45.362,-121.697],zoom_start=4,tiles='Stamen Terrain')
+map=folium.Map(location=[df['LAT'].mean(),df['LON'].mean()],zoom_start=6,tiles='Stamen Terrain')
+def color(elev):
+    minimum=int(min(df['ELEV']))
+    step=int((max(df['ELEV'])-min(df['ELEV']))/3)
+    if elev in range(minimum,minimum+step):
+        col="green"
+    elif elev in range(minimum+step,minimum+step*2):
+        col="white"
+    elif elev in range(minimum+step*2,minimum+step*3):
+        col='orange'
+    else:
+        col='red'
+    return col
 
 #create all markers
 for lat,lon,name,elev in zip(df['LAT'],df['LON'],df['NAME'],df['ELEV']):
-    map.simple_marker(location=[lat,lon],popup=name, marker_color='green' if elev in range(0,1000) else 'white' if elev in range(1000,2000) else 'orange' if elev in range(2000,3000) else 'red')
+    map.simple_marker(location=[lat,lon],popup=name,marker_color=color(elev))
 
 # add Portland for fun
 map.simple_marker(location=[45.5231,-122.6765],popup='Portland, OR', marker_color='white')
